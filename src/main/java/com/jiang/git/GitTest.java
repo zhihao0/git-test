cd65923ac7512161986fc3e2c11362c2cdf4ac06
jiangzhihao <1554416437@qq.com>
2021-08-27 09:34:11 +0800

    second commit

--- src/main/java/com/jiang/git/GitTest.java
+++ src/main/java/com/jiang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.jiang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1");
+        System.out.println("hello git 2");
     }
 }
